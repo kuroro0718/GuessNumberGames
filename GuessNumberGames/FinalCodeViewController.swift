1f374864718d248607c01ea037683b0efc89a4c7
Alex Liang <u9211011@gmail.com>
2016-08-09 14:23:59 +0800

    Add final code UI element and corresponding outlet

--- GuessNumberGames/FinalCodeViewController.swift
+++ GuessNumberGames/FinalCodeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FinalCodeViewController.swift
 //  GuessNumberGames
 //
 //  Created by ycliang on 2016/8/9.
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FinalCodeViewController: UIViewController {
 
+    @IBOutlet weak var playerNameLabel: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-
+    
 }
-
