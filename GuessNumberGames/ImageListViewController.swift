b68f8a62ad95487c284d935c45d2cf088842fadd
Alex Liang <u9211011@gmail.com>
2016-08-11 15:28:30 +0800

    Add Tab Bar controller and image view page

---
+++ GuessNumberGames/ImageListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ImageListViewController.swift
+//  GuessNumberGames
+//
+//  Created by ycliang on 2016/8/11.
+//  Copyright © 2016年 AlexLiang. All rights reserved.
+//
+
+import UIKit
+
+class ImageListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
